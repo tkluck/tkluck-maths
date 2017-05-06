7a345d20e2a535208ceb56a2e590f1f602575996
Timo Kluck <tkluck@infty.nl>
2017-05-06 15:10:25 +0200

    Remove pedantic (co)domain dimensions in type definition

--- lib/Modules.jl
+++ lib/Modules.jl
@@ -2,20 +2,24 @@ module Modules
 
 using Polynomials: _Polynomial, _reduce, groebner_basis, syzygies
 
-export Morphism, kernel
+export ModuleMorphism, HomspaceMorphism, lift, kernel
 
-type Morphism{P <: _Polynomial, DimDomain, DimCodomain}
+abstract Morphism{P <: _Polynomial}
+
+type ModuleMorphism{P <: _Polynomial} <: Morphism{P}
     m::Matrix{P}
 end
 
+type HomspaceMorphism{P <: _Polynomial} <: Morphism{P}
+    m::Array{P, 4}
+end
 
-function lift{P <: _Polynomial, DimDomain, DimCodomain}(F::Morphism{P, DimDomain, DimCodomain}, x::Vector{P})::Nullable{Matrix{P}}
 
-    assert(length(x) == DimCodomain)
+function lift{P <: _Polynomial}(F::Morphism{P}, x::Vector{P})::Nullable{Matrix{P}}
 
     F_vectors = [
         getindex(F.m, :, i)
-        for i in 1:DimDomain
+        for i in indices(F.m, 2)
     ]
     (basis, transformation) = groebner_basis(F_vectors)
     (x_red, factors) = _reduce(x, basis)
@@ -43,10 +47,10 @@ function lift{P <: _Polynomial}(F::Vector{P}, x::P)::Nullable{Matrix{P}}
 
 end
 
-function kernel{P <: _Polynomial, DimDomain, DimCodomain}(F::Morphism{P, DimDomain, DimCodomain})
+function kernel{P <: _Polynomial}(F::Morphism{P})
     F_vectors = [
         getindex(F.m, :, i)
-        for i in 1:DimDomain
+        for i in indices(F.m, 2)
     ]
     (basis, transformation) = groebner_basis(F_vectors)
     S = syzygies(basis)
--- test.jl
+++ test.jl
@@ -22,15 +22,14 @@ println( syzygies(basis) * basis )
 
 
 using Modules
-using Modules: lift
 
-F = Morphism{typeof(x),2,2}([x 0; 0 y])
+F = ModuleMorphism{typeof(x)}([x 0; 0 y])
 
 println(lift([x, -y], 3*x + x*y))
 
 println(lift(F, [2*x; x*y]))
 
-G = Morphism{typeof(x),2,2}([x x*y; y y^2])
+G = ModuleMorphism{typeof(x)}([x x*y; y y^2])
 println(kernel(G))
 
 println(G.m * kernel(G))
