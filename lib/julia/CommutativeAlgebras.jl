99c047e9e902121d93e0d618051d3098004d167f
Timo Kluck <tkluck@infty.nl>
2017-10-22 21:08:33 +0200

    A few more fixes for groebner_basis->groebner_transformation

--- lib/julia/CommutativeAlgebras.jl
+++ lib/julia/CommutativeAlgebras.jl
@@ -2,8 +2,8 @@ module CommutativeAlgebras
 
 using Nulls
 
+using PolynomialRings
 using PolynomialRings.Polynomials: Polynomial
-using PolynomialRings.Groebner: groebner_basis
 using PolynomialRings.Util: lazymap
 
 abstract type _AbstractCommutativeAlgebra{P<:Polynomial} end
@@ -37,13 +37,13 @@ Ideal(generators::Polynomial...) = Ideal(collect(generators), null, null)
 generators(I::Ideal) = I.generators
 function _grb(I::Ideal)
     if isnull(I._grb)
-        I._grb, I._trns = groebner_basis(I.generators)
+        I._grb, I._trns = groebner_transformation(I.generators)
     end
     I._grb
 end
 function _trns(I::Ideal)
     if isnull(I._grb)
-        I._grb, I._trns = groebner_basis(I.generators)
+        I._grb, I._trns = groebner_transformation(I.generators)
     end
     I._trns
 end
