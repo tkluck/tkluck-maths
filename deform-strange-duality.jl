4131793fe39474e4a15e076736084ec569d599c8
Timo Kluck <tkluck@infty.nl>
2018-02-20 13:00:16 +0100

    Some M.F. moved to .Library

--- deform-strange-duality.jl
+++ deform-strange-duality.jl
@@ -1,9 +1,9 @@
-using PolynomialRings; using MFDeformations; using MatrixFactorizations; using OrbifoldEquivalence; using CommutativeAlgebras
+using PolynomialRings; using MFDeformations; using MatrixFactorizations.Library; using OrbifoldEquivalence
 using FlintNumbers
 
 @ring! ℚ[a,b,c]
-Q, f1,f2, g = MatrixFactorizations.E14_Q10_matrix_and_equations()
-W = MatrixFactorizations.E14_Q10()
+Q, f1,f2, g = MatrixFactorizations.Library.E14_Q10_matrix_and_equations()
+W = MatrixFactorizations.Library.E14_Q10()
 f = f1*f2
 
 F = FlintNumberField(@ring(ℚ[a,b,c])/Ideal(f1,g,c))
