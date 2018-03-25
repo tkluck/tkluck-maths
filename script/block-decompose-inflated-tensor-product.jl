3cfdcd7ae86ea678e9147d2ed045ccea9a81b48c
Timo Kluck <tkluck@infty.nl>
2018-03-25 15:04:29 +0200

    Add computation for I(y^3-x^3)⨶I(z^3-y^3)

--- script/block-decompose-inflated-tensor-product.jl
+++ script/block-decompose-inflated-tensor-product.jl
@@ -49,11 +49,82 @@ iszero(XX*At + At*XX)
 
 e = -(λ⊗Jacobian) * At
 
-iszero(e^2 - e)
+# e is a morphism
+Q*e == e*Q
 
-iszero(Q*e - e*Q)
+# e is idempotent up to homotopy
+hhh = (matrix_over_t(λ, :y, 2)*∇( matrix_over_t(λ, :y, 2) * ∇(matrix_over_t(X, :y, 2))))(y=0)
+e^2 == e + Q*hhh + hhh*Q
 
-block_diagonalization(Q)
+"""
+Newton's method: define e1 = e + Q*b + b*Q, and solve for b s.t.
+
+e1^2 - e1 = o(b)
+
+This gives
+
+{e - 1//2, b} = hhh
+
+(anti-commutator on the LHS).
+"""
+function solve_anti_commutator(A,h)
+    basis = []
+    for i in eachindex(A)
+        b = zeros(eltype(A), size(A)...)
+        b[i] = one(eltype(A))
+        push!(basis, b)
+    end
+    image_basis = map(b->A*b + b*A, basis)
+
+    gr, T = gröbner_transformation(image_basis)
+
+    factors, r = divrem(h, gr)
+    if !iszero(r)
+        return nothing
+    else
+        return (factors*T)*basis
+    end
+end
+
+b1 = solve_anti_commutator(e - one(e)//2, hhh)
+e1 = e + Q*b1 + b1*Q
+e1^2  == e1
+"""
+WOW! We only need one iteration of Newton's method.
+
+e1's image is spanned by the vectors:
+"""
+Im = hcat(gröbner_basis([e1[:,i] for i in indices(e1,2)])...)
+"""
+and expressed on this basis, e1 is the identity
+"""
+e1*Im == Im
+"""
+The interesting one is Q:
+"""
+QQ=[
+0                  0           -x^2      -3z;
+0                  0           -(x+z)//3   1;
+x-z            3x*z - 3z^2      0          0;
+(x^2-z^2)//3   (x^2*z - x^3)    0          0;
+]
+
+Q*Im == Im*QQ
+
+QQ^2
+"""
+Yay! So QQ is the finite dimensional representative of the homotopy class of
+A⨶B.
+
+Next step: find the "unitor", the map identifying A with QQ (since B is the unit
+matrix factorization). Note that the dimensions are different, so I'm guessing
+that identification up to homotopy is good enough?
+"""
+block_diagonalization(QQ)
+"""
+So it's A ⊕ "trivial factorization". Am I okay with that? E.g.
+is the trivial factorization contractible (homotopy equiv to zero)?
+"""
 
 # Knörrer perdiodicity
 S = @ring! ℚ[u,v]
