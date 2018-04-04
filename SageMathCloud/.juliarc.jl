853313a948160db26d8512d71822883c2ba0ef97
Timo Kluck <tkluck@infty.nl>
2018-04-04 16:06:36 +0200

    Remove Revise from juliarc
    It doesn't seem to work and I don't want to investigate now. In any
    case, I'm not installing it in the binder, so it would be broken there.

--- SageMathCloud/.juliarc.jl
+++ SageMathCloud/.juliarc.jl
@@ -3,9 +3,3 @@ unshift!(LOAD_PATH, "$DIR/lib/julia")
 
 # for Jupyter notebook on my screen
 ENV["COLUMNS"] = 120
-
-# I almost always want this, but don't error out if it
-# break for e.g. https://github.com/timholy/Revise.jl/issues/77
-try
-    using Revise
-end
