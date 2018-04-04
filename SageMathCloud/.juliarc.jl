38da182dd6f3474e23071457c7d63a96307e79c1
Timo Kluck <tkluck@infty.nl>
2018-04-04 13:53:15 +0200

    Additions to juliarc.jl

--- SageMathCloud/.juliarc.jl
+++ SageMathCloud/.juliarc.jl
@@ -1,2 +1,11 @@
 DIR=dirname(dirname(realpath(expanduser(("~/.juliarc.jl")))))
 unshift!(LOAD_PATH, "$DIR/lib/julia")
+
+# for Jupyter notebook on my screen
+ENV["COLUMNS"] = 120
+
+# I almost always want this, but don't error out if it
+# break for e.g. https://github.com/timholy/Revise.jl/issues/77
+try
+    using Revise
+end
